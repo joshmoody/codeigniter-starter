c305550479c080fbb73a6edcef93f670cf653cd2
Josh Moody <jgmoody@gmail.com>
2013-03-30 22:09:11 -0500

    Added https://github.com/appleboy/CodeIgniter-Native-Session to the default setup.

---
+++ application/config/session.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Session namespace
+$config['sess_namespace'] = 'codeigniter-starter';
+
+/* End of file session.php */
+/* Location: ./application/config/session.php */
